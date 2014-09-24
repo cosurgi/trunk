29d30e120ae920daf372894c73033adb1d76abb4
Janek Kozicki <janek@kozicki.pl>
2014-09-24 14:26:28 +0200

    Quantum wave function starts to work (but needs cleanup).

--- pkg/qm/WaveFunctionState.cpp
+++ pkg/qm/WaveFunctionState.cpp
@@ -8,6 +8,7 @@ YADE_PLUGIN(
 	(QuantumMechanicalState)
 	(WaveFunctionState)
 	(GaussianWavePacket)
+	(GaussianAnalyticalPropagatingWavePacket_1D)
 	(WaveFunctionGeometry)
 	(WavePacketParameters)
 	(WaveFunctionInteractionPhysics)
@@ -17,7 +18,8 @@ YADE_PLUGIN(
 	(Law2_WaveFunctionInteractionGeometry_WaveFunctionInteractionPhysics_WaveFunctionInteractionPhysics)
 	(SchrodingerKosloffPropagator)
 	#ifdef YADE_OPENGL
-	(Gl1_WaveFunctionGeometry)
+	(Gl1_GaussianAnalyticalPropagatingWavePacket_1D)
+//	(Gl1_WaveFunctionGeometry)
 	(Gl1_WaveFunctionInteractionPhysics)
 	#endif	
 	);
@@ -53,6 +55,17 @@ CREATE_LOGGER(GaussianWavePacket);
 // !! at least one virtual function in the .cpp file
 GaussianWavePacket::~GaussianWavePacket(){};
 
+CREATE_LOGGER(GaussianAnalyticalPropagatingWavePacket_1D);
+// !! at least one virtual function in the .cpp file
+GaussianAnalyticalPropagatingWavePacket_1D::~GaussianAnalyticalPropagatingWavePacket_1D(){};
+
+std::complex<Real> GaussianAnalyticalPropagatingWavePacket_1D::waveFunctionValue_1D_positionRepresentation
+	(Real x,Real x0,Real t,Real k0,Real m, Real a, Real h)
+{
+	x -= x0;
+	return exp(-(m*x*x+Mathr::I*a*a*k0*(k0*h*t-2.0*m*x)/(2.0*a*a*m+2.0*Mathr::I*h*t)))/(pow(Mathr::PI,0.25)*(pow(a+Mathr::I*h*t/(a*m),0.25)));
+};
+
 /*********************************************************************************
 *
 * W A V E   F U N C T I O N   G E O M E T R Y
@@ -73,7 +86,7 @@ WavePacketParameters::~WavePacketParameters(){};
 
 /*********************************************************************************
 *
-* W A V E   F U N C T I O N   I N T E R A C T I O N   P H Y S I C S   (constitutive parameters of the contact)
+* W A V E   F U N C T I O N   I N T E R A C T I O N   P H Y S I C S     (constitutive parameters of the contact)
 *
 *********************************************************************************/
 CREATE_LOGGER(WaveFunctionInteractionPhysics);
@@ -130,17 +143,72 @@ SchrodingerKosloffPropagator::~SchrodingerKosloffPropagator(){};
 *
 *********************************************************************************/
 #ifdef YADE_OPENGL
+	#include<yade/pkg/common/GLDrawFunctors.hpp>
 	#include<yade/lib/opengl/OpenGLWrapper.hpp>
 	#include<yade/lib/opengl/GLUtils.hpp>
 
-	CREATE_LOGGER(Gl1_WaveFunctionGeometry);
-	bool Gl1_WaveFunctionGeometry::abs=true;
-	bool Gl1_WaveFunctionGeometry::real=false;
-	bool Gl1_WaveFunctionGeometry::imag=false;
-	Gl1_WaveFunctionGeometry::~Gl1_WaveFunctionGeometry(){};
-	void Gl1_WaveFunctionGeometry::go(const shared_ptr<Shape>&, const shared_ptr<State>&,bool,const GLViewInfo&)
+	// FIXME: probably should be merged with Gl1_WaveFunctionGeometry, but let's do something that works first.
+	CREATE_LOGGER(Gl1_GaussianAnalyticalPropagatingWavePacket_1D);
+	bool Gl1_GaussianAnalyticalPropagatingWavePacket_1D::abs=true;
+//	bool Gl1_GaussianAnalyticalPropagatingWavePacket_1D::real=false;
+//	bool Gl1_GaussianAnalyticalPropagatingWavePacket_1D::imag=false;
+	Gl1_GaussianAnalyticalPropagatingWavePacket_1D::~Gl1_GaussianAnalyticalPropagatingWavePacket_1D(){};
+//	void Gl1_GaussianAnalyticalPropagatingWavePacket_1D::go
+//	(const shared_ptr<Shape>& shape, const shared_ptr<State>& state,bool wire,const GLViewInfo& glv)
+//	{
+//		std::cerr<<"Gl1_GaussianAnalyticalPropagatingWavePacket_1D\n";
+//		glColor3v(shape->color);
+////		GaussianAnalyticalPropagatingWavePacket_1D* packet=(dynamic_cast<GaussianAnalyticalPropagatingWavePacket_1D>)(state.get());
+//		glutSolidCube(0.2);
+//	};
+
+void Gl1_GaussianAnalyticalPropagatingWavePacket_1D::go(const shared_ptr<Shape>& shape, const shared_ptr<State>& state,bool wire,const GLViewInfo&)
+{
+	//Vector3r &extents = (static_cast<Box*>(cg.get()))->extents;
+	Vector3r extents(0.1,0.2,0.3);
+	GaussianAnalyticalPropagatingWavePacket_1D* packet=dynamic_cast<GaussianAnalyticalPropagatingWavePacket_1D*>(state.get());
+//	std::cerr<<"\nGl1_GaussianAnalyticalPropagatingWavePacket_1D\n";
+	glScalef(2*extents[0],2*extents[1],2*extents[2]);
+//	if (wire)
+//	{
+//		glutWireCube(1);
+//	} else {
+//		glutSolidCube(1);
+//	}
+	glBegin(GL_LINE_STRIP);
+	glColor3v(shape->color);
+	for(Real x=-32.0 ; x<32.0 ; x+=0.1 )
 	{
+//	(Real x,Real x0,Real t,Real k0,Real m, Real a, Real h)
+		std::complex<Real> y = packet->waveFunctionValue_1D_positionRepresentation(x,packet->x0,0,packet->k0,packet->m,packet->a,packet->hbar);
+		glVertex3d(x,std::real(y),0);
 	}
+	glColor3v(shape->color);
+	for(Real x=-32.0 ; x<32.0 ; x+=0.1 )
+	{
+//	(Real x,Real x0,Real t,Real k0,Real m, Real a, Real h)
+		std::complex<Real> y = packet->waveFunctionValue_1D_positionRepresentation(x,packet->x0,0,packet->k0,packet->m,packet->a,packet->hbar);
+		glVertex3d(x,std::imag(y),0);
+	}
+	glColor3v(shape->color);
+	for(Real x=-32.0 ; x<32.0 ; x+=0.1 )
+	{
+//	(Real x,Real x0,Real t,Real k0,Real m, Real a, Real h)
+		std::complex<Real> y = packet->waveFunctionValue_1D_positionRepresentation(x,packet->x0,0,packet->k0,packet->m,packet->a,packet->hbar);
+		glVertex3d(x,std::abs(y),0);
+	}
+	glEnd();
+};
+
+
+//	CREATE_LOGGER(Gl1_WaveFunctionGeometry);
+//	bool Gl1_WaveFunctionGeometry::abs=true;
+//	bool Gl1_WaveFunctionGeometry::real=false;
+//	bool Gl1_WaveFunctionGeometry::imag=false;
+//	Gl1_WaveFunctionGeometry::~Gl1_WaveFunctionGeometry(){};
+//	void Gl1_WaveFunctionGeometry::go(const shared_ptr<Shape>&, const shared_ptr<State>&,bool,const GLViewInfo&)
+//	{
+//	}
 
 	CREATE_LOGGER(Gl1_WaveFunctionInteractionPhysics);
 	bool Gl1_WaveFunctionInteractionPhysics::abs=true;
