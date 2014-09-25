92db16ef3c613c86dbd67d2bf7db27e3366756d7
Janek Kozicki <janek@kozicki.pl>
2014-09-25 11:00:29 +0200

    Add a FIXME to Sphere.hpp regarding +bug/528509, we need to discuss it.

--- pkg/common/Sphere.hpp
+++ pkg/common/Sphere.hpp
@@ -5,6 +5,10 @@
 // XXX: we need to think more about introducing yade namespace, to avoid such name conflicts, see https://gitlab.com/yade-dev/trunk/issues/57
 // see comments there for explanation
 namespace yade{
+//FIXME - either put everything in yade namespace, or make some #define when compiling with gengeo
+//        currently doxygen is lost when generating documentation, and Cylinder & ChainedCylinder
+//        are not on the inheritance diagrams
+//        Or maybe namespace names based on package name? (dem,lbm,qm..?)
 
 class Sphere: public Shape{
 	public:
