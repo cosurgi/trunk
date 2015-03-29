3638e59c725cea7d4da4b0f6ae5519b4afff7d81
Janek Kozicki <janek@kozicki.pl>
2015-03-29 21:45:37 +0200

    Add drop down menu QComboBox in qt4 interface
    To use it #include<py/wrapper/Menu.hpp>. Also see documentation, section
    `Attribute registration`.

---
+++ py/wrapper/Menu.cpp
@@ -0,0 +1,10 @@
+// 2015 © Janek Kozicki <cosurgi@gmail.com>
+#include<py/wrapper/Menu.hpp>
+std::string menuSelection(Menu m)
+{
+	if(m.size()==0)
+		return "";
+	else
+		return m[0].substr(8);
+};
+
