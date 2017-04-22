6d744edb08e435eb683b880d7f53bf6b429b5853
Janek Kozicki <janek@kozicki.pl>
2017-04-22 20:50:04 +0200

    Liczy NaRb z przesuniętym potencjałem oraz z tłumieniem. Ładniej rysuje. Tłumienie jest złe przy tej małej siatce.

--- examples/qm/1d-potential-from-file.py
+++ examples/qm/1d-potential-from-file.py
@@ -2,29 +2,38 @@
 # -*- coding: utf-8 -*-
 
 dimensions= 1
-startX    = -100  # -500
-end__X    =  100  #  500
+startX    = -55 # -100  # -500
+end__X    =  55 #  100  #  500
+
+zero_shift_left  = -45
+energy_shift_val = -0.067908708333333
+
+dampMarginBandMin = 2
+dampMarginBandMax = 10
+dampFormula       = 0   # 0 - uses exp() with smoothed edge, 1 - uses 'traditional' exp() with discontinuity
+dampExponent      = 4
+
 size1d    = end__X-startX
 halfSize  = [size1d/2,0.1,0.1]# must be three components, because yade is inherently 3D and uses Vector3r. Remaining components will be used for AABB
 # The grid size must be a power of 2 to allow FFT. Here 2**13=8192 is used.
-gridSize  = 2**11 # 2**16
+gridSize  = 2**12 # 2**11 # 2**16
 
 # wavepacket parameters
 k0_x       = 0
-gaussWidth = 0.95 #/2.0
-x0_center  = 6.15
+gaussWidth = 0.95 #/ 2.0
+x0_center  = 6.15 + zero_shift_left
 mass       = 1604.391494
 
 # potential parameters
 potentialCenter1  = [(startX+end__X)*0.5,0,0]
 potentialHalfSize = halfSize
 potentialFileName = "LiH_pot.txt"
-potentialColumnX  = 1
-potentialColumnVal= 2
+potentialColumnX  = 19
+potentialColumnVal= 20
 
 # draw scale (was potentialValue)
-potentialDrawScale  = 50  # 5000
-potentialDrawminusZ = -10 # -340
+potentialDrawScale  = 15000  # 5000
+potentialDrawminusZ = 0 # -340
 psiDrawScale        = 30
 
 O.engines=[
@@ -41,14 +50,22 @@ O.engines=[
 		[Law2_QMIGeom_QMIPhysFromFile()]
 	),
 	#SchrodingerAnalyticPropagator(),
-	SchrodingerKosloffPropagator(threadNum=32),
+	SchrodingerKosloffPropagator(
+             FIXMEatomowe_MASS = mass
+            ,dampMarginBandMin = dampMarginBandMin
+            ,dampMarginBandMax = dampMarginBandMax
+            ,dampFormula       = dampFormula
+            ,dampExponent      = dampExponent
+            ,dampDebugPrint    = True
+            ,threadNum         = 1
+            ),
 	PyRunner(iterPeriod=1,command='myAddPlotData()'),
 	PyRunner(iterPeriod=10,command='savePlotData()')
 ]
 
-displayOptionsPot= { 'partAbsolute':['default wire', 'hidden', 'nodes', 'points', 'wire', 'surface']
+displayOptionsPot= { 'partAbsolute':['default hidden', 'hidden', 'nodes', 'points', 'wire', 'surface']
                     ,'partImaginary':['default hidden', 'hidden', 'nodes', 'points', 'wire', 'surface']
-                    ,'partReal':['default hidden', 'hidden', 'nodes', 'points', 'wire', 'surface']
+                    ,'partReal':['default surface', 'hidden', 'nodes', 'points', 'wire', 'surface']
 		    ,'stepRender':["default stripes","hidden","frame","stripes","mesh"]}
 
 stepRenderHide   =["default hidden","hidden","frame","stripes","mesh"]
@@ -84,7 +101,14 @@ potentialBody1.shape     = QMGeometry(extents=potentialHalfSize,displayOptions=[
                                          QMDisplayOptions(partsScale=potentialDrawScale
                                         ,renderSe3=(Vector3(0,0,potentialDrawminusZ), Quaternion((1,0,0),0))
                                         ,**displayOptionsPot)])
-potentialBody1.material  = QMParametersFromFile(dim=dimensions,hbar=1,filename=potentialFileName,columnX=potentialColumnX,columnVal=potentialColumnVal)
+potentialBody1.material  = QMParametersFromFile(
+                                          dim=dimensions
+                                         ,hbar=1
+                                         ,filename=potentialFileName
+                                         ,columnX=potentialColumnX
+                                         ,shiftX=zero_shift_left
+                                         ,shiftVal=energy_shift_val
+                                         ,columnVal=potentialColumnVal)
 potentialBody1.state     = QMStPotentialFromFile(se3=[potentialCenter1,Quaternion((1,0,0),0)])
 O.bodies.append(potentialBody1)
 
@@ -92,7 +116,8 @@ O.bodies.append(potentialBody1)
 #O.dt=.000001
 #O.dt=.001
 #O.dt=.2
-O.dt=1
+#O.dt=1
+O.dt=6
 
 ## Save the scene to file, so that it can be loaded later. Supported extension are: .xml, .xml.gz, .xml.bz2.
 O.save('/tmp/a.xml.bz2');
@@ -103,18 +128,19 @@ O.save('/tmp/a.xml.bz2');
 ############################################
 
 from yade import plot
-plot.plots={'t':('re','im','abs')}
+plot.plots={'t':('re','im','abs','population')} #,'pim')}
 
 def myAddPlotData():
 	symId=0
 	numId=1
 	O.bodies[symId].state.update()
 	#psiDiff=((O.bodies[symId].state)-(O.bodies[numId].state))
-	autocorr = ((O.bodies[symId].state)|(O.bodies[numId].state))
-	plot.addData(t=O.time,re=autocorr.real,im=autocorr.imag,abs=abs(autocorr) )
+	autocorr   = ((O.bodies[symId].state)|(O.bodies[numId].state))
+	population = ((O.bodies[numId].state)|(O.bodies[numId].state))
+	plot.addData(t=O.time,re=autocorr.real,im=autocorr.imag,abs=abs(autocorr),population=population.real ) #, pim=population.imag )
 
 def savePlotData():
-	plot.saveDataTxt('testy_cols_'+str(potentialColumnX)+'_'+str(potentialColumnVal)+'.txt')
+	plot.saveDataTxt('dt='+str(O.dt)+'_gaussWidth='+str(gaussWidth)+'_cols_'+str(potentialColumnX)+'_'+str(potentialColumnVal)+'.txt')
 
 plot.liveInterval=.2
 plot.plot(subPlots=False)
@@ -127,7 +153,7 @@ try:
 	#Gl1_QMGeometry().analyticUsesStepOfDiscrete=False
 	qt.View()
 	#qt.controller.setWindowTitle("Packet inside a 1D well")
-	qt.controller.setWindowTitle("col_x="+str(potentialColumnX)+' col_y='+str(potentialColumnVal))
+	qt.controller.setWindowTitle('dt='+str(O.dt)+'_gaussWidth='+str(gaussWidth)+' col_x='+str(potentialColumnX)+' col_y='+str(potentialColumnVal))
 	qt.controller.setViewAxes(dir=(0,1,0),up=(0,0,1))
 	qt.views()[0].center(False,5) # median=False, suggestedRadius = 5
 except ImportError:
